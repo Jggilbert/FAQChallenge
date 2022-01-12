cee87eafaf837450a2826a3ed07a98d783726b29
Jggilbert <75323365+Jggilbert@users.noreply.github.com>
2022-01-12 01:32:35 -0600

    added font changes to btn, added gradient but still working that out

--- faq-accordion-card-main/script.js
+++ faq-accordion-card-main/script.js
@@ -15,9 +15,4 @@ var i;
   }
 }
 
-// window.onclick = myFunction;
 
-// // If the user clicks in the window, set the background color of <body> to yellow
-// function myFunction() {
-//   document.getElementsByTagName("BODY")[0].style.backgroundColor = "yellow";
-// }
